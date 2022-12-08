1ba59039f30741123dc4e2674e3e43d296389859
Artjom Kurapov <artkurapov@gmail.com>
2022-12-08 03:04:46 +0200

    add filtering, sorting, grouping

---
+++ README.md
@@ -0,0 +1,29 @@
+# express-axios-api-composition-sandbox
+
+
+### Specification
+
+The acceptance criteria for the required feature would be to create a RESTful GET endpoint that returns the session
+and media with combined details relevant to the consumer of the API.
+
+The structure of the RESTful GET endpoint would be defined by you, but it should meet the following criteria:
+✅. Fix the media context in the response using `/sessions/:sessionId/media` and `/media-context/:sessionId` endpoints to have only relevant and corrected media as a result
+✅. Group media by the context type
+✅. Irrelevant media must be filtered out
+✅. Media list must be sorted by probability descending
+5. Implement at least one test, which must pass on every run
+
+
+### Usage
+http://localhost:3000/api/sessions/90d61876-b99a-443e-994c-ba882c8558b6
+
+### Installation
+```sh
+npm install
+npm start
+```
+   
+### Testing
+```sh
+npm test
+```
--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,53 @@ import './externalService';
 const app = express();
 const port = 3000;
 
+type UUID = string;
+type MediaContext = {
+  id: UUID
+  mediaId: UUID
+  context: string
+  probability: number
+}
+type Media = {
+  id: UUID,
+  mimeType: string
+  context: string
+}
+
 app.get('/api/sessions/:sessionId', async (req, res) => {
   try {
     const sessionId = req.params.sessionId;
-    const [session, media, context] = await Promise.all([
-      axios.get(`https://api.veriff.internal/sessions/${sessionId}`),
+    const [media, context] = await Promise.all([
+      //axios.get(`https://api.veriff.internal/sessions/${sessionId}`),
       axios.get(`https://api.veriff.internal/sessions/${sessionId}/media`),
       axios.get(`https://api.veriff.internal/media-context/${sessionId}`)
     ]);
 
-    return res.status(200).json({
-      session: session.data,
-      media: media.data,
-      context: context.data
-    });
+
+    const mediaSet: Map<string, Media> = new Map();
+
+    for(const row of media.data as Media[]){
+      mediaSet.set(row.id, row)
+    }
+
+    const filteredData = (context.data as MediaContext[])
+      .filter(obj => obj.context !== "none")
+      .sort((a, b) => b.probability - a.probability)
+      .map((row:MediaContext)=>{
+        return {
+          ...row,
+          mimeType: mediaSet.get(row.mediaId)?.mimeType
+        }
+      }).reduce((acc, cur) => {
+        const key = cur.context;
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        acc[key].push(cur);
+        return acc;
+      }, {});
+    return res.status(200).json(filteredData);
+
   } catch (error: any) {
     console.error('Error happened', error);
     return res.status(error.response.status).json({ message: error.response.data });
